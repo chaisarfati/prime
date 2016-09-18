f5bddaaa726a131823aef8b7e9e813a49ee730a0
Chris Vig <chris@invictus.so>
2016-09-18 18:16:07 -0500

    Add template main program class.

---
+++ src/main/java/so/invictus/template/Program.java
@@ -0,0 +1,22 @@
+/**
+ * Program.java
+ * @author Chris Vig (chris@invictus.so)
+ */
+
+package so.invictus.template;
+
+/**
+ * Main program class.
+ */
+public class Program
+{
+
+  /**
+   * Main program entry point.
+   */
+  public static void main(String[] args)
+  {
+    System.out.println("Hello world!");
+  }
+
+}
